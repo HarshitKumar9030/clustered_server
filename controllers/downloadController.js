13e33e07f0a38a2f5f6df3f6ab8047b41ab012da
Harshit Kumar <harshitkumar9030@gmail.com>
2024-06-27 15:10:55 +0530

    added stream 10x download speeds!!!

--- controllers/downloadController.js
+++ controllers/downloadController.js
@@ -1,9 +1,9 @@
 const ytdl = require("ytdl-core");
 const ffmpeg = require("fluent-ffmpeg");
 const { formatExtensions, stopWords } = require("../utils/constants");
-const { rateLimiter } = require("../utils/rateLimiter");
 const { TrendingWord } = require("../models/TrendingWord");
 const { DownloadLog } = require("../models/DownloadLog");
+const stram = require('stream');
 
 const downloadVideo = async (req, res) => {
   const videoURL = req.query.url;
@@ -43,6 +43,7 @@ const downloadVideo = async (req, res) => {
     );
 
     const stream = ytdl.downloadFromInfo(info, { format: chosenFormat });
+    const passThrough = new stram.PassThrough();
 
     if (format === "wav") {
       ffmpeg(stream)
@@ -54,6 +55,7 @@ const downloadVideo = async (req, res) => {
             res.status(500).send("Error processing video");
           }
         })
+        .pipe(passThrough)
         .pipe(res, { end: true });
     } else if (format === "flac") {
       ffmpeg(stream)
@@ -65,52 +67,55 @@ const downloadVideo = async (req, res) => {
             res.status(500).send("Error processing video");
           }
         })
+        .pipe(passThrough)
         .pipe(res, { end: true });
-      } else if (format === "mp4" || format === "webm") {
-        ffmpeg(stream)
-          .videoCodec("copy")
-          .format(format)
-          .on("error", (err) => {
-            console.error("Error processing video:", err);
-            if (!res.headersSent) {
-              res.status(500).send("Error processing video");
-            }
-          })
-          .pipe(res, { end: true });
-      } else {
-        ffmpeg(stream)
-          .audioCodec("libmp3lame")
-          .audioBitrate(quality === "highest" ? 320 : 128) 
-          .format("mp3")
-          .on("error", (err) => {
-            console.error("Error processing video:", err);
-            if (!res.headersSent) {
-              res.status(500).send("Error processing video");
-            }
-          })
-          .pipe(res, { end: true });
-      }
-
-      // Record the video title for trending topics
-      const words = title
-        .toLowerCase()
-        .replace(/[^\w\s]/g, "")
-        .split(" ")
-        .filter((word) => !stopWords.includes(word) && word.length > 2);
-
-      for (const word of words) {
-        await TrendingWord.updateOne(
-          { word },
-          { $inc: { count: 1 } },
-          { upsert: true }
-        );
-      }
-    } catch (err) {
-      console.error("Error downloading video:", err);
-      if (!res.headersSent) {
-        res.status(500).send("Error downloading video");
-      }
+    } else if (format === "mp4" || format === "webm") {
+      ffmpeg(stream)
+        .videoCodec("copy")
+        .format(format)
+        .on("error", (err) => {
+          console.error("Error processing video:", err);
+          if (!res.headersSent) {
+            res.status(500).send("Error processing video");
+          }
+        })
+        .pipe(passThrough)
+        .pipe(res, { end: true });
+    } else {
+      ffmpeg(stream)
+        .audioCodec("libmp3lame")
+        .audioBitrate(quality === "highest" ? 320 : 128)
+        .format("mp3")
+        .on("error", (err) => {
+          console.error("Error processing video:", err);
+          if (!res.headersSent) {
+            res.status(500).send("Error processing video");
+          }
+        })
+        .pipe(passThrough)
+        .pipe(res, { end: true });
     }
-  };
+
+    // Record the video title for trending topics
+    const words = title
+      .toLowerCase()
+      .replace(/[^\w\s]/g, "")
+      .split(" ")
+      .filter((word) => !stopWords.includes(word) && word.length > 2);
+
+    for (const word of words) {
+      await TrendingWord.updateOne(
+        { word },
+        { $inc: { count: 1 } },
+        { upsert: true }
+      );
+    }
+  } catch (err) {
+    console.error("Error downloading video:", err);
+    if (!res.headersSent) {
+      res.status(500).send("Error downloading video");
+    }
+  }
+};
 
 module.exports = { downloadVideo };
